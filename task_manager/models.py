01744cb9d20abcd7ff711208e056506db79a533e
Artem <apbogolyubov@gmail.com>
2020-07-14 20:13:25 +0300

    Update models.py & Update admin.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+
+    def __str__(self):
+        return self.title

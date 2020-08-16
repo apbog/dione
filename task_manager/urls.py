6835da903727f64f993eb084241fdd03f91da9c5
Artem <apbogolyubov@gmail.com>
2020-08-16 08:07:55 +0300

    Change /task/command to /command Add List view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('task/create/', views.CreateTask.as_view()),
+    path('create/', views.CreateTask.as_view()),
+    path('list/', views.ListTask.as_view()),
 ]

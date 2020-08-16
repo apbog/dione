7473c11a9f4bd2cdce399dd2eea36d761a192a8a
Artem <apbogolyubov@gmail.com>
2020-08-16 08:07:48 +0300

    Change /task/command to /command Add List view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from task_manager.serializers import TaskSerializer
 from rest_framework import generics
+from task_manager.models import Task
 
 
 # Create your views here.
 class CreateTask(generics.CreateAPIView):
     serializer_class = TaskSerializer
+
+
+class ListTask(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()

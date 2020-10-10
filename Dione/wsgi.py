236892a91e00dfe2c447d4d11d5fcea95e26dae3
Artem <apbogolyubov@gmail.com>
2020-10-10 09:41:32 +0000

    append from AWS

--- Dione/wsgi.py
+++ Dione/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for Dione project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/Dione')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_project/Dione/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dione.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dione.settings')
 
 application = get_wsgi_application()

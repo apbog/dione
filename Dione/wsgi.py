04544ab92c1074f560bdaac9cda836154ee40171
Artem <apbogolyubov@gmail.com>
2020-10-10 12:44:00 +0300

    Change settings to aws

--- Dione/wsgi.py
+++ Dione/wsgi.py
@@ -1,10 +1,8 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/opt/bitnami/apps/django/django_projects/Dione')
 os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_project/Dione/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dione.settings")
-
-from django.core.wsgi import get_wsgi_application
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dione.settings_aws")
 application = get_wsgi_application()

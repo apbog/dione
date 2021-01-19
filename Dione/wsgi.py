00a75548e3ce9c784aae4b0435c73f6f3230da38
apbog <apbogolyubov@gmail.com>
2021-01-19 22:31:51 +0300

    Delete AWS files

--- Dione/wsgi.py
+++ Dione/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/Dione')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_project/Dione/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dione.settings")
-application = get_wsgi_application()
+# sys.path.append('/opt/bitnami/apps/django/django_projects/Dione')
+# os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_project/Dione/egg_cache")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Dione.settings")
+# application = get_wsgi_application()

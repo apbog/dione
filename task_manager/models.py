43907af8fd8652ab250cec3c7a6009b9dd70558b
apbog <apbogolyubov@gmail.com>
2021-01-20 10:56:03 +0300

    Add "created" field to task model

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title

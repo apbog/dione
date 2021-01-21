4d5ace9c55f733e3a9060ba5dc6db8f2a31e1c74
apbog <apbogolyubov@gmail.com>
2021-01-21 10:26:44 +0300

    Rename url

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='landing_page'),
+    path('', views.home_page, name='home_page'),
     path('landing/', views.landing_page, name='landing_page'),
 ]

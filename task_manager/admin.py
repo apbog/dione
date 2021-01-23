40af6791b262da864cac2c4a03a6a91f9ff2503e
apbog <apbogolyubov@gmail.com>
2021-01-23 22:14:19 +0300

    Update TaskAdmin model

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -5,7 +5,9 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    fields = ('title', 'text')
     list_display = ['title', 'text', 'created']
+    list_filter = ('created',)
     search_fields = ['title', 'text']
     date_hierarchy = 'created'
     readonly_fields = ['title']

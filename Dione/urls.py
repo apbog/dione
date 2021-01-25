52d5058d910320d5f1b05525fc816d16f1328b56
apbog <apbogolyubov@gmail.com>
2021-01-25 21:56:51 +0300

    Update urls

--- Dione/urls.py
+++ Dione/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/task_manager/', include('task_manager.urls')),
     path('', include('landing.urls')),

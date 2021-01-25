6a544e151516b57a6c80fe3a87a50229d5422568
apbog <apbogolyubov@gmail.com>
2021-01-25 21:57:24 +0300

    Add login_required decorator

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from task_manager.serializers import TaskSerializer
 from rest_framework import generics
@@ -5,15 +7,18 @@ from task_manager.models import Task
 
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
 class CreateTask(generics.CreateAPIView):
     serializer_class = TaskSerializer
 
 
+@method_decorator(login_required, name='dispatch')
 class ListCreateTask(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
 
+@method_decorator(login_required, name='dispatch')
 class ListTask(generics.ListAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()

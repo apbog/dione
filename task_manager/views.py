398117f19a6425deef339b61fef4fdb70a355690
apbog <apbogolyubov@gmail.com>
2021-01-26 23:18:11 +0300

    Edit views

--- task_manager/views.py
+++ task_manager/views.py
@@ -11,14 +11,24 @@ from task_manager.models import Task
 class CreateTask(generics.CreateAPIView):
     serializer_class = TaskSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
 
 @method_decorator(login_required, name='dispatch')
 class ListCreateTask(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
 
 
 @method_decorator(login_required, name='dispatch')
 class ListTask(generics.ListAPIView):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)

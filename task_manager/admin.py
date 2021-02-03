61e849b3a294e879c7e583ae87f54425bf923c49
apbog <apbogolyubov@gmail.com>
2021-02-03 19:19:51 +0300

    Change admin Task model

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -5,10 +5,9 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    fields = ('title', 'text', 'created', 'created_by')
-    list_display = ('title', 'text', 'created', 'created_by')
-    list_filter = ('created', 'created_by')
+    fields = ('title', 'text', 'created', 'created_by', 'is_active')
+    list_display = ('title', 'text', 'created', 'created_by', 'is_active')
+    list_filter = ('created', 'created_by', 'is_active')
     search_fields = ('title', 'text')
     date_hierarchy = 'created'
     readonly_fields = ('created', 'created_by')
-    # TODO: Add new field to ADM

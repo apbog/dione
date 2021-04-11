fc32fd5cf2f8d848a111089503254ab84c642e60
apbog <apbogolyubov@gmail.com>
2021-04-11 14:00:24 +0300

    Параметры настроек вынесены в env

--- Dione/settings.py
+++ Dione/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't=f5m#obm485_b&kbd&804)64hpac3u!=)27*0gwq7(g8yp+9('
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition

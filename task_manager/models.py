b817d267fcddaf1509bd79bc1df870869c1ffb55
apbog <apbogolyubov@gmail.com>
2021-04-11 16:33:04 +0300

    Add last_modified field to task

--- task_manager/models.py
+++ task_manager/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, editable=False, null=True, blank=True, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
 

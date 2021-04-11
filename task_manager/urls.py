57cb07e912891a250324793c2900e9dc39495e11
apbog <apbogolyubov@gmail.com>
2021-04-11 16:38:26 +0300

    Add Update & Delete commands

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('create/', views.CreateTask.as_view()),
     path('list_create/', views.ListCreateTask.as_view()),
     path('list/', views.ListTask.as_view()),
+    path('retrieve/<int:pk>/', views.RetrieveTask.as_view()),
+    path('update/<int:pk>/', views.UpdateTask.as_view()),
+    path('task/<int:pk>/', views.EditTask.as_view()),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -25,3 +25,26 @@ class ListTask(generics.ListAPIView):
 
     def get_queryset(self):
         return Task.objects.filter(created_by=self.request.user)
+
+
+class RetrieveTask(generics.RetrieveAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
+
+
+class UpdateTask(generics.UpdateAPIView):
+    serializer_class = TaskSerializer
+    lookup_field = 'pk'
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
+
+
+class EditTask(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    lookup_field = 'pk'
+
+    def get_queryset(self):
+        return Task.objects.filter(created_by=self.request.user)
